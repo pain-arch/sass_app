d48d2de1b3164b6fef2e06ba19435d7bd34a1f36
pain-arch <himulol123@gmail.com>
2024-07-02 02:05:07 +0600

    Refactor view and template file names for consistency

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -10,7 +10,7 @@ def home_page_view(request, *args, **kwargs):
     my_context ={
         "page_title": my_title
     }
-    html_template = "home.html"
+    html_template = "base.html"
     #html_file_path = this_dir / "home.html"
     #html_ = html_file_path.read_text()
     return render(request, html_template, my_context)
