a6b9b1a556c3bf9b0e0ce23637ac5305905477eb
pain-arch <himulol123@gmail.com>
2024-07-02 01:49:11 +0600

    chore: Add templates directory and update settings to include it

---
+++ src/cfehome/views.py
@@ -0,0 +1,16 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    my_title = "Home Page"
+    my_context ={
+        "page_title": my_title
+    }
+    html_template = "home.html"
+    #html_file_path = this_dir / "home.html"
+    #html_ = html_file_path.read_text()
+    return render(request, html_template, my_context)
